1347744d9e7c5dd0572002326d3934ba331df627
Adrian <kowal.adrian@interia.pl>
2020-07-19 13:39:09 +0200

    Initial commit

---
+++ awkward-back/src/main/java/pl/awkward/AwkwardBackApplication.java
@@ -0,0 +1,13 @@
+package pl.awkward;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwkwardBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwkwardBackApplication.class, args);
+    }
+
+}
---
+++ awkward-back/src/test/java/pl/awkward/AwkwardBackApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.awkward;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwkwardBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
