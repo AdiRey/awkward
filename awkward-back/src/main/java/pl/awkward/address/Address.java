bdd9049bed7962fc90cd5e4d56e10143db606767
Adrian <kowal.adrian@interia.pl>
2020-07-19 15:39:13 +0200

    Edit address entity and repo

--- awkward-back/src/main/java/pl/awkward/address/Address.java
+++ awkward-back/src/main/java/pl/awkward/address/Address.java
@@ -1,12 +1,13 @@
 package pl.awkward.address;
 
 import lombok.Data;
+import pl.awkward.shared.BaseEntity;
 
 import javax.persistence.*;
 
 @Entity
 @Data
-public class Address {
+public class Address implements BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -14,4 +15,6 @@ public class Address {
     private String country;
     @Column(nullable = false, length = 80)
     private String city;
+    @Column(columnDefinition = "boolean default true")
+    private Boolean active;
 }
