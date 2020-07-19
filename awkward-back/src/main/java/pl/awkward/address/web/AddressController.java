263aa2b13af163676b3cdf86a4655e70bbc1f3b8
Adrian <kowal.adrian@interia.pl>
2020-07-19 17:18:04 +0200

    Edit AddressController

--- awkward-back/src/main/java/pl/awkward/address/web/AddressController.java
+++ awkward-back/src/main/java/pl/awkward/address/web/AddressController.java
@@ -18,22 +18,31 @@ public class AddressController extends BaseCrudController<Address> {
 
     private final BaseConverter<Address, AddressDto> addressConverter;
     private final BaseConverter<Address, AddressCreateUpdateDto> addressCreateUpdateConverter;
+    private final AddressService addressService;
 
 
     public AddressController(BaseRepository<Address> repository,
                              BaseConverter<Address, AddressDto> addressConverter,
-                             BaseConverter<Address, AddressCreateUpdateDto> addressCreateUpdateConverter) {
+                             BaseConverter<Address, AddressCreateUpdateDto> addressCreateUpdateConverter,
+                             final AddressService addressService) {
         super(repository);
         this.addressConverter = addressConverter;
         this.addressCreateUpdateConverter = addressCreateUpdateConverter;
+        this.addressService = addressService;
     }
 
     @GetMapping("")
     public ResponseEntity<Page<AddressDto>> getAll(@RequestParam(defaultValue = "0") final int page,
                                                    @RequestParam(defaultValue = "20") final int size,
                                                    @RequestParam(defaultValue = "id") final String column,
-                                                   @RequestParam(defaultValue = "ASC") final String direction) {
-        return super.getAll(page, size, column, direction, this.addressConverter.toDto());
+                                                   @RequestParam(defaultValue = "ASC") final String direction,
+                                                   @RequestParam(defaultValue = "") final String filter) {
+        if (filter.equals(""))
+            return super.getAll(page, size, column, direction, this.addressConverter.toDto());
+        return ResponseEntity
+                .ok(this.addressService.getAllWithFilter(page, size, column, direction, filter)
+                        .map(this.addressConverter.toDto())
+                );
     }
 
     @GetMapping("/{id}")
@@ -43,6 +52,7 @@ public class AddressController extends BaseCrudController<Address> {
 
     @PostMapping("")
     public ResponseEntity<Void> create(@RequestBody @Valid final AddressCreateUpdateDto dto) {
+        this.addressService.acceptableCountryAndCity(dto.getCountry(), dto.getCity());
         return super.create(dto, this.addressCreateUpdateConverter.toEntity());
     }
 
@@ -51,6 +61,11 @@ public class AddressController extends BaseCrudController<Address> {
         return super.delete(id);
     }
 
-
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> update(@PathVariable final Long id, @RequestBody @Valid final AddressCreateUpdateDto dto) {
+        this.addressService.acceptableCountryAndCity(dto.getCountry(), dto.getCity());
+        boolean status = this.addressService.update(id, this.addressCreateUpdateConverter.toEntity().apply(dto));
+        return super.update(status);
+    }
 
 }
