7502756803c0e89f2bff1c4276f63af404a4ccc6
Adrian <kowal.adrian@interia.pl>
2020-07-19 19:38:01 +0200

    Small edit Gender

--- awkward-back/src/main/java/pl/awkward/gender/Gender.java
+++ awkward-back/src/main/java/pl/awkward/gender/Gender.java
@@ -3,16 +3,13 @@ package pl.awkward.gender;
 import lombok.Data;
 import pl.awkward.shared.BaseEntity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Data
 public class Gender implements BaseEntity {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(unique = true, nullable = false, length = 10)
     private String gender;
