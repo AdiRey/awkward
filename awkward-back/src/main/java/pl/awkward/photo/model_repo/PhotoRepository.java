35144ba18ddaf52e7e3353a45d1fd3cf32f9b5f6
Adrian <kowal.adrian@interia.pl>
2020-07-21 11:08:15 +0200

    Add photo entity and repo

--- awkward-back/src/main/java/pl/awkward/photo/model_repo/PhotoRepository.java
+++ awkward-back/src/main/java/pl/awkward/photo/model_repo/PhotoRepository.java
@@ -1,6 +1,12 @@
 package pl.awkward.photo.model_repo;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import pl.awkward.shared.BaseRepository;
 
+import java.util.Optional;
+
 public interface PhotoRepository extends BaseRepository<Photo> {
+    Page<Photo> findAllByUserIdAndIsActiveOrderByAddDateDesc(Long userId, Boolean isActive, Pageable pageable);
+    Optional<Photo> findByPath(String path);
 }
